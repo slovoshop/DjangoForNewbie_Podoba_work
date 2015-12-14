2e78cec9db1ec2fe3e8e3ffb26b1deab6e65f893
Alex Strutynskyy <kominterna62@gmail.com>
2015-12-14 14:57:05 +0200

    groups_list dynamization

--- students/views.py
+++ students/views.py
@@ -37,7 +37,18 @@ def students_delete(request, sid):
 # Views for Groups
 
 def groups_list(request):
-  return HttpResponse('<h1>Groups Listing</h1>')
+  groups = (
+    {'id'       : 1,
+    'name'      : u'МтМ-21',
+    'leader'    : u'Ячменев Олег'},
+    {'id'       : 2,
+    'name'      : u'МтМ-22',
+    'leader'    : u'Віталій Подоба'},
+    {'id'       : 3,
+    'name'      : u'МтМ-23',
+    'leader'    : u'Іванов Андрій'},
+  )
+  return render(request, 'students/groups_list.html', {'groups': groups})
 
 def groups_add(request):
   return HttpResponse('<h1>Group Add Form</h1>')
