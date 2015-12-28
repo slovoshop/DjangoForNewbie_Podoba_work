19a544792fc789fe42e9ac118e8a025141762fbe
Alex Strutynskyy <kominterna62@gmail.com>
2015-12-28 15:27:51 +0200

    Chapter7 id-sorting

--- students/views/students.py
+++ students/views/students.py
@@ -14,15 +14,17 @@ def students_list(request):
 		# try to order students list
 		order_by = request.GET.get('order_by', '')
 		if order_by == '':
-			students = students.order_by('last_name') 
+			students = students.order_by('last_name') 				
 		elif order_by == 'ticket':
-			students = students.extra(select={'ticket': 'CAST(ticket AS UNSIGNED)'}).extra(order_by = ['ticket'])
+			students = students.extra(select={'ticket': 'CAST(ticket AS UNSIGNED)'}).extra(order_by = ['ticket'])					
 		elif order_by in ('id', 'last_name', 'first_name'):
 			students = students.order_by(order_by)
 		if request.GET.get('reverse', '') == '1':
-			students = students.reverse()
+			students = students.reverse()			
+		
+		per_page = 3		
 
-		paginator = Paginator(students, 3)
+		paginator = Paginator(students, per_page)
 		page = request.GET.get('page')
 		try:
 			students = paginator.page(page)
@@ -34,8 +36,19 @@ def students_list(request):
 			# last page of results.
 			students = paginator.page(paginator.num_pages)
 
+		# Добавка к forloop.counter при сортировке по полю №
+		id_sort = per_page * (students.number - 1)
+	
+		# формирование списка номеров для поля № при реверсной сортировке
+		id_reverse = []				
+		if request.GET.get('reverse', '') == '1':		
+			for i in range(paginator._count - id_sort, paginator._count - id_sort - per_page, -1):
+					id_reverse.append(i)	
+		
+		test = 'none'		
+
 		return render(request, 'students/students_list.html',
-		{'students': students})
+		{'students': students, 'id_sort': id_sort, 'id_reverse': id_reverse, 'test': test})
 
 
 def students_add(request):
