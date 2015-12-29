2361db6ae14f343e16b0ba8e4acbbd9f9d81fd71
Alex Strutynskyy <kominterna62@gmail.com>
2015-12-29 20:01:42 +0200

    chapter8_p276-298 students_add without validation

--- students/views/students.py
+++ students/views/students.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models.students import Student
+from ..models.groups import Group
 
 
 # Views for Students
@@ -51,8 +53,54 @@ def students_list(request):
 		{'students': students, 'id_sort': id_sort, 'id_reverse': id_reverse, 'test': test})
 
 
+
+
 def students_add(request):
-  return HttpResponse('<h1>Student Add Form</h1>')
+
+	# was form posted?
+	if request.method == "POST":		
+
+		# was form add button clicked?
+		if request.POST.get('add_button') is not None:
+
+			# TODO: validate input from user
+			errors = {}
+
+			if not errors:
+				# create student object
+				student = Student(
+					first_name=request.POST['first_name'],
+					last_name=request.POST['last_name'],
+					middle_name=request.POST['middle_name'],
+					birthday=request.POST['birthday'],
+					ticket=request.POST['ticket'],
+					student_group=
+					Group.objects.get(pk=request.POST['student_group']),
+					photo=request.FILES['photo'],
+					notes=request.POST['notes'],
+					)
+				# save it to database
+				student.save()
+				# redirect user to students list
+				return HttpResponseRedirect(reverse('home'))
+
+			else:
+				# render form with errors and previous user input
+				return render(request, 'students/students_add.html',
+				{'groups': Group.objects.all().order_by('title'),
+				'errors': errors})
+
+		elif request.POST.get('cancel_button') is not None:
+			# redirect to home page on cancel button
+			return HttpResponseRedirect(reverse('home'))
+
+	else:
+		# initial form render
+		return render(request, 'students/students_add.html', 
+		{'groups': Group.objects.all().order_by('title')})
+
+
+
 
 def students_edit(request, sid):
   return HttpResponse('<h1>Edit Student %s</h1>' % sid)
